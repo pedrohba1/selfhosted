da570ade54a59d4b9eb7015a6e32e4924a724fb8
Pedro <pedro@semiotic.ai>
2026-03-27 22:12:14 -0300

    zsh, tools in rpi04

--- defaults/rpi04-configuration.nix
+++ defaults/rpi04-configuration.nix
@@ -10,6 +10,7 @@
       "wheel"
       "video"
     ];
+    shell = pkgs.zsh;
     # Add your public SSH key here so you can log in immediately.
     openssh.authorizedKeys.keys = [ ];
 
@@ -20,12 +21,45 @@
   };
 
   environment.systemPackages = with pkgs; [
-    vim
+    git
+    networkmanager
+    curl
+    htop
     tree
+    vim
   ];
 
   # Uncomment or add modules and services you need below:
   services.openssh.enable = true;
   services.networkmanager.enable = true;
   # hardware.pulseaudio.enable = true;
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    initExtra = ''
+      # The following lines were added by compinstall
+      zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
+      zstyle :compinstall filename "$HOME/.zshrc"
+
+      autoload -Uz compinit
+      compinit
+      # End of lines added by compinstall
+      # Lines configured by zsh-newuser-install
+      HISTFILE=~/.histfile
+      HISTSIZE=1000
+      SAVEHIST=1000
+      setopt beep
+      bindkey -v
+      # End of lines configured by zsh-newuser-install
+
+      # PATH additions (append/prepend without clobbering)
+      typeset -Ua path
+      path=("$HOME/.npm-packages/bin" $path)   # prepend npm bin
+      path+=("$HOME/.foundry/bin")             # append foundry
+
+      export COLORTERM=truecolor
+      export PATH="$HOME/.local/bin:$PATH"
+    '';
+  };
 }
